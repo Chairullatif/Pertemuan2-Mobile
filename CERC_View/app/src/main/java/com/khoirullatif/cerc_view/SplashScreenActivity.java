b8b8911afd3038d7624715584aa9f6d0509a3496
Chairullatif Aji Sadewa <khoirullatif337@gmail.com>
2020-10-01 16:26:42 +0700

    Menambah project

---
+++ CERC_View/app/src/main/java/com/khoirullatif/cerc_view/SplashScreenActivity.java
@@ -0,0 +1,32 @@
+package com.khoirullatif.cerc_view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 2000L);
+    }
+}
+
